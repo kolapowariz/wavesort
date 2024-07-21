20e3020ecd2d35fb1f76ccd72ebf1a6c17cd2a22
Kolapo Wariz <kolapowariz@gmail.com>
2024-07-21 18:32:22 +0100

    new

---
+++ app/dashboard/editor/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useState } from "react"
+import ReactMarkdown from 'react-markdown'
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css'
+
+const mdParser = new MarkdownIt();
+
+export default function Editor() {
+  const [markdown, setmarkdown] = useState<string>('');
+
+  const handleEditorChange = ({ text }: { text: string }) => {
+    setmarkdown(text);
+  }
+  return (
+    <div>
+      <h1>Markdown Editor</h1>
+      <MdEditor value={markdown} onChange={handleEditorChange} renderHTML={(text) => <ReactMarkdown>{text}</ReactMarkdown> } className="h-full grid grid-cols-1" />
+    </div>
+  )
+}
