aed8c260d9a66c11675bfd563cb24db4111e2507
Kolapo Wariz <kolapowariz@gmail.com>
2024-07-26 10:16:25 +0100

    worked on landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -24,21 +24,21 @@ export default function LandingPage() {
                 <h1 className="text-7xl md:text-9xl">waveSort</h1>
             </div> : <>
                 <section className="md:hidden">
-                    <Image src='/next.svg' alt="Logo" width={60} height={40} />
+                    <Image src='/next.svg' alt="Logo" width={60} height={40} className="dark:bg-green-50" />
                     <div className="flex flex-col items-center">
                         <h1 className="uppercase w-52 mt-4 mb-8 text-2xl text-center">Create and access content all in one place.</h1>
                         <Image src={nft} alt="Logo" width={400} height={400} className="mb-8 rounded-full" />
 
                         <Link href='/signup'>
-                            <Button className="w-72 bg-teal-400 p-2 mb-2 rounded-full h-12 text-white text-lg">Create an Account</Button>
+                            <Button className="w-72 bg-[#09233c] p-2 mb-2 rounded-full h-12 text-white text-lg">Create an Account</Button>
                         </Link>
                         <Link href='/login'>
-                            <Button className="w-72 bg-teal-400 mb-2 rounded-full h-10 border border-teal-400 text-white text-lg">Login In</Button>
+                            <Button className="w-72 bg-[#09233c] mb-2 rounded-full h-10 border border-teal-400 text-white text-lg">Log In</Button>
 
 
                         </Link>
 
-                        <p className="w-80 text-sm p-2 mb-10 text-center">By signing up, you are agreeing to our <a href="" className="text-red-400">Terms of service</a> and <a href="" className="text-red-400">Public Policy.</a> </p>
+                        <p className="w-80 text-sm p-2 mb-10 text-center">By signing up, you are agreeing to our <Link className="text-blue-300" href="">Terms of service</Link> and <Link className="text-blue-300" href="">Public Policy.</Link> </p>
                     </div>
                 </section>
 
@@ -47,35 +47,35 @@ export default function LandingPage() {
                         <nav className="flex justify-between items-center">
                             <div className="">
 
-                                <Image src='/next.svg' alt="Logo" width={400} height={400} priority />
+                                <Image src='/next.svg' alt="Logo" width={400} height={400} priority className="dark:bg-green-50" />
                             </div>
                             <ul className="flex justify-around">
-                                <li className="p-2 mr-2"><a href="">Home</a></li>
-                                <li className="p-2 mr-2"><a href="">Our Services</a></li>
-                                <li className="p-2"><a href="">Directory</a></li>
+                                <li className="p-2 mr-2"><Link href="">Home</Link></li>
+                                <li className="p-2 mr-2"><Link href="">Our Services</Link></li>
+                                <li className="p-2"><Link href="">Directory</Link></li>
                             </ul>
                             <div className="">
 
                                 <Link href='/login' ><Button className="mr-8">Log In</Button></Link>
-                                <Link href='/signup'><Button className="mr-2 bg-teal-400 p-2 rounded-md text-white">Sign Up</Button></Link>
+                                <Link href='/signup'><Button className="mr-2 bg-[#09233c] p-2 rounded-md text-white">Sign Up</Button></Link>
                             </div>
                         </nav>
                     </header>
                     <main>
                         <section className="flex justify-between items-center mt-16">
                             <div className="w-72 ml-2">
-                                <p className="text-4xl font-bold">Over <span className="text-teal-400">150,000</span> Medical Articles Written & Peer Reviewed</p>
-                                <span className="mt-2 text-gray-500 block text-base">By peer-reviewing every article posted on Healler, you are sure of every post.</span>
+                                <p className="text-4xl font-bold">Over <span className="text-blue-400">10,000</span>  Articles Written</p>
+                                <span className="mt-2 text-gray-500 block text-base">By peer-reviewing every article posted on waveSort, you are sure of every post.</span>
                             </div>
-                            <div className="w-64 h-80 bg-teal-200 rounded-xl mr-2"></div>
+                            <div className="w-64 h-80 bg-[#09233c] rounded-xl mr-2"></div>
                         </section>
-                        <section className="bg-teal-200 mt-24 flex justify-around items-center">
+                        <section className="bg-[#09233c] mt-24 flex justify-around items-center">
                             <div>
                                 <Image src='/next.svg' alt="Logo" width={40} height={40} />
                             </div>
                             <div className="w-80 mr-4">
-                                <p className="text-4xl font-bold">Join Healler Today</p>
-                                <span className="text-gray-500 block text-sm text-center mt-2 mb-4">By peer-reviewing every article posted on Healler, you are sure of every post.</span>
+                                <p className="text-3xl font-bold">Join waveSort Today</p>
+                                <span className="text-gray-500 block text-sm text-center mt-2 mb-4">By peer-reviewing every article posted on waveSort, you are sure of every post.</span>
                                 <div className="flex justify-between items-center">
                                     <Image src='/next.svg' alt="Logo" width={40} height={40} />
                                     <div className="flex flex-col">
