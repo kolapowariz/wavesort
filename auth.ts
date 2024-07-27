b55329004c8b7abb1104238fc94785e9db38ef1c
Kolapo Wariz <kolapowariz@gmail.com>
2024-07-27 18:48:03 +0100

    authentication

---
+++ auth.ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import bcrypt from 'bcryptjs';
+import { getUser } from '@/app/lib/action';
+
+const nextAuth = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        console.log('credentials', credentials);
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        const user = await getUser(email);
+        console.log('user', user);
+        if (!user) {
+          return null;
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        console.log('passwordMatch', passwordMatch);
+
+        if (passwordMatch) {
+          console.log('logged in');
+          return user;
+        }
+
+        console.log('invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
+ 
+export const { handlers, signIn, signOut, auth } = nextAuth;
