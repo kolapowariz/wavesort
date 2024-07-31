d4682f414e1a4634c5641a4a53fa9ae5177a8189
Kolapo Wariz <kolapowariz@gmail.com>
2024-07-31 20:11:37 +0100

    modified main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import LandingPage from '@/components/LandingPage';
-import { ModeToggle } from "@/components/toggle";
 
 export default function Page() {
   return (
     <div className="m-4">
-      <div className="mt-2">
-            <ModeToggle />
-          </div>
-      {/* <LandingPage/> */}
-      <h1 className='text-center text-5xl mt-40'>COMING SOON</h1>
+      <LandingPage />
     </div>
   );
 }
